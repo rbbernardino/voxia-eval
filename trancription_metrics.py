1d095cc6aea83689661f7b7ff4e710da0d535a73
Rodrigo Barros Bernardino <rbbernardino@gmail.com>
2019-07-19 19:59:05 -0300

    finished implementation of test script

---
+++ trancription_metrics.py
@@ -0,0 +1,111 @@
+###############################################
+##               DISCLAIMER                  ##
+## Code adapted from                         ##
+##     https://github.com/lw4z/Similarities  ##
+###############################################
+
+from jiwer import wer
+import nltk 
+import distance
+import re, math
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk import tokenize
+import argparse
+import os
+from pathlib import Path
+
+
+class CompareTexts:
+    def __init__(self):
+        self.WORD = re.compile(r'\w+')
+        self.sws = stopwords.words('portuguese')
+
+    # Stopwords removal
+    def _text_normalized(self, text):
+        palavras_tokenize = tokenize.word_tokenize(text, language='portuguese')
+        filtered_sentence  = list(filter(lambda x: x.lower() not in self.sws, palavras_tokenize))
+        return " ".join(filtered_sentence)
+
+    # Cosine
+    def _get_cosine_result(self, vec1, vec2):
+        intersection = set(vec1.keys()) & set(vec2.keys())
+        numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+        sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+        sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+        denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+        if not denominator:
+            return 0.0
+        else:
+            return float(numerator) / denominator
+
+    def _text_to_vector(self, text):
+        words = self.WORD.findall(text)
+        return Counter(words)
+
+    def _get_cosine(self, text1, text2):
+        vector1 = self._text_to_vector(text1)
+        vector2 = self._text_to_vector(text2)
+        cosine = self._get_cosine_result(vector1, vector2)
+        return cosine
+
+    # Jaccard
+    def _get_jaccard(self, text1, text2):
+        jaccard = nltk.jaccard_distance(set(text1), set(text2))
+        return jaccard
+
+    # Levenshtein
+    def _get_levenshtein(self, text1, text2):
+        levenshtein = distance.levenshtein(text1, text2)
+        return levenshtein
+
+    # Word Error Rate
+    def _get_wer(self, text1, text2):
+        return wer(text1, text2)
+
+    # Punctuations
+    def _get_pontuation(self, text):
+        numberOfFullStops = 0
+        numberOfQuestionMarks= 0
+        numberOfExclamationMarks= 0
+        numberOfCommaMarks = 0
+        numberOfColonMarks= 0
+        numberTotalPunctuation = 0
+
+        for line in text:
+            numberOfFullStops += line.count(".")
+            numberOfQuestionMarks += line.count("?")
+            numberOfExclamationMarks += line.count("!")
+            numberOfCommaMarks += line.count(",")
+            numberOfColonMarks += line.count(":")
+
+        numberTotalPunctuation = numberOfFullStops + numberOfCommaMarks + numberOfQuestionMarks + numberOfExclamationMarks + numberOfColonMarks
+        return numberOfFullStops, numberOfCommaMarks, numberOfQuestionMarks, numberOfExclamationMarks, numberOfColonMarks, numberTotalPunctuation
+
+    def compare_texts(self, test_text, gt_text):
+        # Get punctuation
+        numberOfPunctuation = self._get_pontuation(test_text)
+
+        # Stopwords removal
+        # test1 = text_normalized(text1)
+        # test2 = text_normalized(text2)
+
+        out_str = ""
+        # Similatities results
+        out_str += "WER:         {:2f}\n".format(self._get_wer(test_text, gt_text))
+        out_str += "Jaccard:     {:2f}\n".format(self._get_jaccard(test_text, gt_text))
+        out_str += "Levenshtein: {}\n"   .format(self._get_levenshtein(test_text, gt_text))
+        out_str += "Cosine:      {:2f}\n".format(self._get_cosine(test_text, gt_text))
+
+        # Punctuation results
+        out_str += '\n'
+        out_str += 'Quantidade de Pontos:           {}\n'.format(numberOfPunctuation[0])
+        out_str += 'Quantidade de Virgulas:         {}\n'.format(numberOfPunctuation[1])
+        out_str += 'Quantidade de Interrogações:    {}\n'.format(numberOfPunctuation[2])
+        out_str += 'Quantidade de Exclamações:      {}\n'.format(numberOfPunctuation[3])
+        out_str += 'Quantidade de Dois Pontos:      {}\n'.format(numberOfPunctuation[4])
+        out_str += 'Quantidade Total de Pontuações: {}\n'.format(numberOfPunctuation[5])
+
+        return out_str
--- voxia_ttest.py
+++ voxia_ttest.py
@@ -16,79 +16,136 @@ If audio file is remote (stored at GS bucket), check config.json file.
 """
 
 import os
+import sys
 import json
 import argparse
+import requests
+from subprocess import run
+from subprocess import PIPE
+from subprocess import CalledProcessError
 from pathlib import Path
+from trancription_metrics import CompareTexts
 
 
 SCRIPT_DIR = Path(os.path.realpath(__file__)).parent
 CONFIG_FILE = SCRIPT_DIR / "config.json"
+API_PATH = "valid_gs"
+RESULTS_PATH = Path("results")
 
 
-def evaluate_transcription(audio_fname, transcribed_str, gt_fname):
-    return 0
+def prepare_outdir(outdir_path: Path):
+    if outdir_path.exists():
+        if outdir_path.is_file():
+            print('ERROR: output path is a file')
+    else:
+        outdir_path.mkdir(parents=True)
+
+
+def str_to_file(out_path: Path, in_str):
+    out_file = open(str(out_path), "w")
+    out_file.write(in_str)
+    out_file.close()
+
+
+def upload_to_bucket(file_path: Path, remote_path: str):
+    # OBS: gsutil cp will print progress to stderr by design, commands like
+    # "ls" will print to stdout
+    cmd = ["gsutil", "cp", str(file_path), remote_path]
+    try:
+        rslt = run(cmd, stderr=sys.stdout)
+        rslt.check_returncode()
+    except CalledProcessError as err:
+        print('ERROR when uploading file to gs bucket:', rslt.stderr.decode('utf-8'))
+        exit(1)
+
+
+def evaluate_transcription(transcribed_str, gt_fname):
+    text_comparator = CompareTexts()
+    gt_str = open(gt_fname, "r").read()
+    return text_comparator.compare_texts(transcribed_str, gt_str)
 
 
 def request_transcription(remote_path, speakers_number, config_dict):
-    # TODO check if voxia api is up and listening
-
-    # TODO send transcribe request to voxia API
+    voxia_ip = config_dict['voxia_ip']
+    voxia_port = config_dict['voxia_port']
+    voxia_url = "http://" + voxia_ip + ":" + voxia_port + "/" + API_PATH
     
-    # TODO handle response (extract text only)
+    request_body = {
+        "gs": remote_path,
+        "transcription_id": "1234",
+        "qtd_of_speakers": int(speakers_number)
+    }
 
-    # TODO return response
+    try:
+        request_result = requests.post(voxia_url, json=request_body)
+    except requests.exceptions.ConnectionError as err:
+        print("ERROR, couldn't connect to voxia, check config.json and if the server is running.")
+        exit(1)
 
-    return 0
+    if request_result.status_code != 200:
+        print("ERROR: request returned " + str(request_result.status_code))
+        print("----------------------")
+        print(request_result.text)
+        exit(1)
+
+    result_json = request_result.json()
+    transcribed_str = result_json['text_summarization'][0]
+    return transcribed_str
 
 
 def process_file(audio_fname, speakers_number, config_dict, is_remote_file, gt_fname):
+    prepare_outdir(RESULTS_PATH)
     remote_path = ""
-    #if is_remote_file:
-        # TODO assemble remote path
-        # remote_path = ...
+    if is_remote_file:
+        print("Remote file selected")
+        remote_path = config_dict['bucket_url'] + "/" + audio_fname
+    else:
+        # local file, upload to bucket and set "remote_path" variable
+        print("Local file selected, will upload before processing...")
+        print("----------------------------------------")
+        file_path = Path(audio_fname)
+        remote_path = config_dict['bucket_url'] + "/" + config_dict['bucket_new_dir'] + file_path.name
+        upload_to_bucket(file_path, remote_path)
+        print("----------------------------------------\n")
 
-    #else: # local file
-        # TODO upload file to bucket using gsutil, loading paths from config dict
-
-        # TODO assemble remote path
-
+    print("Requesting transcription...")
     transcribed_str = request_transcription(remote_path, speakers_number, config_dict)
 
     out_transcribed_fname = Path(audio_fname).stem + "_transcribed.txt"
-    out_trans_file = open(out_transcribed_fname, "w")
-    out_trans_file.write(transcribed_str)
-    out_trans_file.close()
+    str_to_file(RESULTS_PATH / out_transcribed_fname, transcribed_str)
+    print("Transcription completed, saved to '{}'".format(RESULTS_PATH / out_transcribed_fname))
 
     if(gt_fname):
-        eval_result_str = evaluate_transcription(audio_fname, transcribed_str, gt_fname)
-        
+        print("\nGT file provided, will evaluate results with several metrics...")
+        eval_result_str = evaluate_transcription(transcribed_str, gt_fname)
         print(eval_result_str)
-        
         out_eval_fname = Path(audio_fname).stem + "_eval.txt"
-        out_eval_file = open(out_eval_fname, "w")
-        out_eval_file.write(eval_result_str)
-        out_eval_file.close()
+        str_to_file(RESULTS_PATH / out_eval_fname, eval_result_str)
+        print("Results saved to '{}'".format(RESULTS_PATH / out_eval_fname))
 
 
 def main():
-    parser = argparse.ArgumentParser(description=
-    """
+    parser = argparse.ArgumentParser(description="""
     Test voxia audio transcription with specified audio file. Writes the processed
     text to file and, if a ground-truth is provided, prints the metrics results.
     If audio file is remote (stored at GS bucket), check config.json file.
     """)
 
     # main parser arguments. The order is respected, so change accordingly.
-    parser.add_argument('AUDIO_FILE', metavar='<AUDIO_FILE>', type=str, help='filename, may be local or remote (check config.json)')
-    parser.add_argument('SPEAKERS_N', metavar='<SPEAKERS_N>', type=str, help='number of speakers')
-    parser.add_argument('-r', '--remote', action='store_true', help='AUDIO_FILE is remote (stored at GS)')
-    parser.add_argument('-g', metavar='<GT_FILE>', type=str, required=False, help='compare result with ground truth text at file GT_FILE')
+    parser.add_argument('AUDIO_FILE', metavar='<AUDIO_FILE>', type=str,
+                        help='filename, may be local or remote (check config.json)')
+    parser.add_argument('SPEAKERS_N', metavar='<SPEAKERS_N>',
+                        type=str, help='number of speakers')
+    parser.add_argument('-r', '--remote', action='store_true',
+                        help='AUDIO_FILE is remote (stored at GS)')
+    parser.add_argument('-g', metavar='<GT_FILE>', type=str, required=False,
+                        help='compare result with ground truth text at file GT_FILE')
 
     args = parser.parse_args()
     audio_fname = args.AUDIO_FILE
     speakers_number = args.SPEAKERS_N
     is_remote_file = args.remote
-    gt_fname = args.g # may be null (=None)
+    gt_fname = args.g  # may be null (=None)
     config_dict = json.load(open(SCRIPT_DIR / "config.json", "r"))
 
     process_file(audio_fname, speakers_number, config_dict, is_remote_file, gt_fname)
